f08fa95d3b83904d560d4180f1b20b5bb2355326
aminkov <aminkov@vmware.com>
2019-09-28 18:11:49 +0300

    Adding map zoom slider

--- app/src/main/java/com/minrax/findmesos/Settings.java
+++ app/src/main/java/com/minrax/findmesos/Settings.java
@@ -4,6 +4,8 @@ import android.content.SharedPreferences;
 import android.content.Intent;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -34,39 +36,64 @@ import androidx.appcompat.app.AppCompatActivity;
         }
 
         private void saveSettingsValues() {
+            //saving default phone numbers
             EditText phone1 = findViewById(R.id.phonenumber1);
             EditText phone2 = findViewById(R.id.phonenumber2);
             //EditText phone3 = findViewById(R.id.phonenumber3);
-            EditText email1 = findViewById(R.id.sendtoemail1);
-            EditText smsmsg = findViewById(R.id.smsMessage);
-            ToggleButton maptype = findViewById(R.id.maptypeswitch);
             String strphone1 = phone1.getText().toString();
             String strphone2 = phone2.getText().toString();
             //String strphone3 = phone3.getText().toString();
-            String strsendtoemail1 = email1.getText().toString();
-            String strSMSMessage = smsmsg.getText().toString();
-            String terrainon = String.valueOf(maptype.isChecked());
             writeToPreference("p1", strphone1);
             writeToPreference("p2", strphone2);
             //writeToPreference("p3", strphone3);
+
+            //saving default e-mail address
+            EditText email1 = findViewById(R.id.sendtoemail1);
+            String strsendtoemail1 = email1.getText().toString();
             writeToPreference("e1", strsendtoemail1);
+
+            //saving SMS message
+            EditText smsmsg = findViewById(R.id.smsMessage);
+            String strSMSMessage = smsmsg.getText().toString();
             writeToPreference("smsMessage", strSMSMessage);
+
+            //Saving map type
+            ToggleButton maptype = findViewById(R.id.maptypeswitch);
+            String terrainon = String.valueOf(maptype.isChecked());
             writeToPreference("terrainon", terrainon);
 
+            //Saving Map ZOOM value
+            SeekBar mapzoom = findViewById(R.id.mapZoomSlider);
+            String mapzoomvalue = String.valueOf(mapzoom.getProgress());
+            writeToPreference("mapzoom", mapzoomvalue);
         }
 
         private void loadSavedSettings() {
             EditText phone1 = findViewById(R.id.phonenumber1);
             EditText phone2 = findViewById(R.id.phonenumber2);
             //EditText phone3 = findViewById(R.id.phonenumber3);
-            EditText email1 = findViewById(R.id.sendtoemail1);
-            EditText smsmsg = findViewById(R.id.smsMessage);
-            ToggleButton maptype = findViewById(R.id.maptypeswitch);
             phone1.setText(getPreferenceValue("p1"));
             phone2.setText(getPreferenceValue("p2"));
             //phone3.setText(getPreferenceValue("p3"));
+
+            //E-mail field
+            EditText email1 = findViewById(R.id.sendtoemail1);
             email1.setText(getPreferenceValue("e1"));
+
+            //SMS message field
+            EditText smsmsg = findViewById(R.id.smsMessage);
             smsmsg.setText(getPreferenceValue("smsMessage"));
+
+            //Map type switch
+            ToggleButton maptype = findViewById(R.id.maptypeswitch);
             maptype.setChecked(Boolean.parseBoolean(getPreferenceValue("terrainon")));
+
+            //Map ZOOM field
+            SeekBar mapzoom = findViewById(R.id.mapZoomSlider);
+            Integer mapZoomValue = Integer.parseInt(getPreferenceValue("mapzoom"));
+            if (mapZoomValue == null) {mapZoomValue=16;} else {mapZoomValue=Integer.parseInt(getPreferenceValue("mapzoom"));}
+            mapzoom.setProgress(mapZoomValue);
+            TextView MapZoomTitleBox = findViewById(R.id.mapzoomtitle);
+            MapZoomTitleBox.setText("Map ZOOM: " + mapZoomValue);
         }
     }
